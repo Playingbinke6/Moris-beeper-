4e8e464ca36ecdd21a89196e9cf575f6ee9b2916
bronzeye <jasonkwajboy@gmail.com>
2024-12-02 16:48:36 -0500

    Update message.h

--- Moris_beeper/message.h
+++ Moris_beeper/message.h
@@ -1,17 +1,15 @@
-#infdef message_h
+
+#ifndef message_h
 #define message_h
 
 #include"memory.h"
 
-enum Mchar{a, b, c, d, e, f, g, h, i, j, k, l, m,
-           n, o, p, q, r, s, t, u, v, w, x, y, z};
-
 class Message : public Memory {
 public:
 
-  Messages();
+  Message();
   Message(unsigned char* inFrom, unsigned char* inTo, unsigned short inPayload, unsigned char inLength);
-  Message(unsigned char* inFrom, unsigned inChar* to, char const* inMessage);
+  Message(unsigned char* inFrom, unsigned char* inTo, char* inMessage);
   void setLength(unsigned char inLength);
   void setTo(unsigned char* inTo);
   void setFrom(unsigned char* inFrom);
@@ -21,13 +19,11 @@ public:
   unsigned char* getFrom();
   unsigned short getPayload();
   char* getPayloadString();
-  char* payloadToString(unsigned short payload, unsigned char length);
 
 protected:
 
-  unsigned short charToCode (Mchar let);
-  char codeToChar(uin8_t code);  
   unsigned short stringToPayload(char const* message);
+  char* payloadToString(unsigned short payload, unsigned char length);
 
 private:
 
@@ -35,120 +31,73 @@ private:
   unsigned char* to;
   unsigned short payload;
   unsigned char length;
+  char* message;
 
-  const uin8_t codes[26] = 
-  {
-    B 00 00 10 11, //A
-    B 11 10 10 10, //B
-    B 11 10 11 10, //C
-    B 00 11 10 10, //D
-    B 00 00 00 10, //E
-    B 10 10 11 10, //F
-    B 00 11 11 10, //G
-    B 10 10 10 10, //H
-    B 00 00 10 10, //I
-    B 10 11 11 11, //J
-    B 00 11 10 11, //K
-    B 10 11 10 10, //L
-    B 00 00 11 11, //M
-    B 00 00 11 10, //N
-    B 00 11 11 11, //O
-    B 10 11 11 10, //P
-    B 11 11 10 11, //Q
-    B 00 10 11 10, //R
-    B 00 10 10 10, //S
-    B 00 00 00 11, //T
-    B 00 10 10 11, //U
-    B 10 10 10 11, //V
-    B 00 10 11 11, //W
-    B 11 10 10 11, //X
-    B 11 10 11 11, //Y
-    B 11 11 10 10, //Z
   };
     
 
 
-  Message(){}
-  Message(unsigned char* inFrom, unsigned char* inTo, unsigned short inPayload, unsigned char inLength)
+  Message::Message(){}
+  
+  Message::Message(unsigned char* inFrom, unsigned char* inTo, unsigned short inPayload, unsigned char inLength)
     :from(inFrom), to(inTo), payload(inPayload), length(inLength){
 
-      message = payloadToString(payload, length);
-      writeMemory((174 + 13n), message);
-      n++;
+      this->message = payloadToString(this->payload, this->length);
       
     }
 
-  Message(unsigned char* inFrom, unsigned inChar* to, char const* inMessage)
+  Message::Message(unsigned char* inFrom, unsigned char* inTo, char* inMessage)
     :from(inFrom), to(inTo), message(inMessage){
 
-    length = strlen(message);
-    payload = stringToPayload(message, length);
-    writeMemory((174 + 13n), message);
-    n++;    
+    this->length = static_cast<unsigned char>(strlen(this->message));
+    
+    this->payload = stringToPayload(this->message);
     
   }
 
-  void setLength(unsigned char inLength){
-    length = inLength;
+  void Message::setLength(unsigned char inLength){
+    this->length = inLength;
   }
 
-  void setTo(unsigned char* inTo){
-    to = inTo;
+  void Message::setTo(unsigned char* inTo){
+    this->to = inTo;
   }
 
-  void setFrom(unsigned char* inFrom){
-    from = inFrom;
+  void Message::setFrom(unsigned char* inFrom){
+    this->from = inFrom;
   }
 
-  void setPayload(unsigned short inPayload){
-    payload = inPayload;
+  void Message::setPayload(unsigned short inPayload){
+    this->payload = inPayload;
   }
 
-  unsigned char getLength(){
-    return length;
+  unsigned char Message::getLength(){
+    return this->length;
   }
 
-  unsigned char* getTo(){
-    return to;
+  unsigned char* Message::getTo(){
+    return this->to;
   }
 
-  unsigned char* getFrom(){
-    return from;
+  unsigned char* Message::getFrom(){
+    return this->from;
   }
 
-  unsigned short getPayload(){
-    return payload;
+  unsigned short Message::getPayload(){
+    return this->payload;
   }
 
-  char* getPayloadString(){
-    return payloadToString(payload, length);
+  char* Message::getPayloadString(){
+    return payloadToString(this->payload, this->length);
   }
 
-protected:
-
-  unsigned short charToCode (Mchar let){
-    return codes[let];
-  }
-
-  char codeToChar(uin8_t code){
-
-    for(int i=0; i < 26; i++){
-
-      if(code == codes[i]){
-
-        return static_cast<Mchar>(i);
-        
-      } 
-    }  
-  }
-
-  unsigned short stringToPayload(char const* message){
+  unsigned short Message::stringToPayload(char const* message){
 
     
     
   }
 
-  char* payloadToString(unsigned short payload, unsigned char length){
+  char* Message::payloadToString(unsigned short payload, unsigned char length){
 
 
     
